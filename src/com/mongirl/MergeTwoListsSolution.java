8931f71d1ea242844b289f331b617f13bbf3c504
Mongirl <1179194166@qq.com>
2019-04-26 21:31:58 +0800

    21. 合并两个有序链表

--- src/com/mongirl/MergeTwoListsSolution.java
+++ src/com/mongirl/MergeTwoListsSolution.java
@@ -50,4 +50,34 @@ public class MergeTwoListsSolution {
         return result;
 
     }
+
+
+    /**
+     * 何臭不要脸指导之后
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode mergeTwoLists111(ListNode l1, ListNode l2) {
+        ListNode result = new ListNode(0);
+        ListNode temp = result;
+        while(l1 != null && l2 != null){
+            if(l1.val < l2.val){
+                temp.next = new ListNode(l1.val);
+                l1 = l1.next;
+            }else{
+                temp.next = new ListNode(l2.val);
+                l2 = l2.next;
+            }
+            temp = temp.next;
+        }
+        if(l1 != null){
+            temp.next = l1;
+        }
+        if(l2 != null){
+            temp.next = l2;
+        }
+        return result.next;
+
+    }
 }
